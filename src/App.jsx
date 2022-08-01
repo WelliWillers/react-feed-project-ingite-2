7925282dd9665e1fe48b4d830b1b472761f29e92
WelliWillers <wellington.willers@gmail.com>
2022-08-01 20:54:04 -0300

    iniciando projeto com vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header/Header"
+
+import './global.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Hello Vite + React!</p>
+    </div>
+  )
+}
+
+export default App
